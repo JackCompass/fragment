166583f80e94c6c0fbc56d20728d18f4a4cd2df6
Anuj <kumaranuj1303@gmail.com>
2022-12-13 01:18:30 +0530

    add jwt strategy

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,13 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Get, UseGuards} from '@nestjs/common';
+import {AuthGuard} from "@nestjs/passport";
+import {JwtAuthGuard} from "../auth/guards/jwt-auth.guard";
 
 @Controller('users')
 export class UsersController {
 
+    @UseGuards(JwtAuthGuard)
+    @Get('greet')
+    greetUser() {
+        return "Hello User"
+    }
 }
